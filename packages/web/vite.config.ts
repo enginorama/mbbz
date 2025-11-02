5e09663237b6409ac8b0683869dd84103c325b40
Christian Hartinger <c.k.hartinger@gmail.com>
2025-11-02 10:08:28 +0000

    Init

---
+++ packages/web/vite.config.ts
@@ -0,0 +1,15 @@
+import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite';
+import vueDevTools from 'vite-plugin-vue-devtools';
+import VueRouter from 'unplugin-vue-router/vite';
+
+export default defineConfig({
+  plugins: [VueRouter(), vue(), tailwindcss(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
