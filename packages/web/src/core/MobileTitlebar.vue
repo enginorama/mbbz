13409be232b6cf8f43be446acdb9bbb26f0bf119
Christian Hartinger <c.k.hartinger@gmail.com>
2026-04-22 20:03:17 +0000

    Unify page titles

--- packages/web/src/core/MobileTitlebar.vue
+++ packages/web/src/core/MobileTitlebar.vue
@@ -3,12 +3,19 @@ import { MenuIcon } from 'lucide-vue-next';
 import Button from './components/ui/button/Button.vue';
 import { useSidebar } from './components/ui/sidebar';
 const { toggleSidebar } = useSidebar();
+
+defineProps<{
+  title?: string;
+}>();
 </script>
 
 <template>
-  <div class="flex h-14 items-center justify-end gap-4 border-b px-4">
+  <div
+    class="absolute top-0 right-0 left-0 flex h-14 items-center gap-4 border-b bg-white px-4 md:hidden"
+  >
     <Button @click="toggleSidebar" variant="ghost" size="icon">
       <MenuIcon></MenuIcon>
     </Button>
+    <div class="flex-1">{{ title }}</div>
   </div>
 </template>
--- packages/web/src/core/components/PageTitle.vue
+++ packages/web/src/core/components/PageTitle.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import MobileTitlebar from '../MobileTitlebar.vue';
+
 defineProps<{
   title: string;
   subtitle?: string;
@@ -6,13 +8,16 @@ defineProps<{
 </script>
 
 <template>
-  <div class="mb-6 flex items-center justify-between">
-    <div>
-      <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight text-balance">{{ title }}</h1>
-      <p v-if="subtitle" class="text-muted-foreground mt-2 text-sm">{{ subtitle }}</p>
-    </div>
-    <div class="mt-4">
-      <slot></slot>
+  <div class="min-h-14">
+    <MobileTitlebar :title="title" />
+    <div class="mb-6 hidden items-center justify-between md:flex">
+      <div>
+        <h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight text-balance">{{ title }}</h1>
+        <p v-if="subtitle" class="text-muted-foreground mt-2 text-sm">{{ subtitle }}</p>
+      </div>
+      <div class="mt-4">
+        <slot></slot>
+      </div>
     </div>
   </div>
 </template>
