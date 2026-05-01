97a9ab7ff59fceeffea23a16824c937aae1e15e5
Christian Hartinger <c.k.hartinger@gmail.com>
2026-05-01 20:23:22 +0000

    Fix typo

--- packages/web/src/ex-native/parsers/CommandParser.ts
+++ packages/web/src/ex-native/parsers/CommandParser.ts
@@ -1,3 +1,3 @@
 import type { DccExCommand } from '../ExNativeTokenizer';
 
-export type CommandParser<T> = (commmand: DccExCommand) => T | undefined | null;
+export type CommandParser<T> = (command: DccExCommand) => T | undefined | null;
